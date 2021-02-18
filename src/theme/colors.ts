a9b490986e7e4fab0f072965e991364b5eaa2a58
Miguel Lomeli <lomelidev@MacBook-Pro-de-Miguel.local>
2021-02-18 10:27:22 -0600

    tt

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -41,7 +41,7 @@ export const darkColors: Colors = {
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
-  input: "#483f5a",
+  input: "#022d40",
   primaryDark: "#175592",
   tertiary: "#353547",
   text: "#FFFFFF",
